093026ccec4156a9caad631dfe1b1d3e3a5289f5
Taj <tajmarie92@gmail.com>
2022-06-27 20:13:34 -0500

    Add project files.

---
+++ IfStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+            {
+                Console.WriteLine("That's too bad! You will have to wait a couple years.");
+            } else if (age == 18)
+            {
+                Console.WriteLine("Phew! Barely made it!");
+            }else
+            {
+                Console.WriteLine("You are allowed to enter.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
